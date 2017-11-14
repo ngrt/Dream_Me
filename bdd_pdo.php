aa91a93f6615fbb6505e23bfa30aecb671ab72c3
Catherine <catherine.chen@coding-academy.fr>
2017-11-14 11:50:40 +0100

    Added Form.php + bdd_pdo.php

---
+++ bdd_pdo.php
@@ -0,0 +1,21 @@
+<?php 
+	define("ERROR_LOG_FILE","error.log");
+
+	$host = "localhost";
+	$port = 3306;
+	$db = "pool_php_rush";
+	$username = "root";
+	$passwd = "root";
+
+	try
+	{
+		$bdd = new PDO("mysql:host=".$host.":".$port.";dbname=".$db, $username, $passwd);
+	}
+	catch (PDOException $e)
+	{
+		error_log($e->getMessage(), 3, ERROR_LOG_FILE);
+		// echo "PDO ERROR: ".$e->getMessage()." storage in ERROR_LOG_FILE\n";
+		echo "Error connection to DB\n";
+		return 0;
+	}
+?>
