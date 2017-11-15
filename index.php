c0af4a1a78b3313b325f3bc33da12e69b9f0f470
Catherine <catherine.chen@coding-academy.fr>
2017-11-15 19:12:28 +0100

    Added display of product name (with links) and pagination

--- index.php
+++ index.php
@@ -41,18 +41,32 @@ require "bdd_pdo.php";
 	{
 		echo "<p><a href='./login.php'>Log in - Sign in</a></p>";
 	}
+// Affichage tableau avec produits avec un maximum de produits par page défini par :
+$results_per_page = 3;
 
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+$start_from = ($page-1) * $results_per_page;
+
+$sql = "SELECT name FROM products ORDER BY ID DESC LIMIT $start_from, ".$results_per_page;
+$result = $bdd->query($sql); 
+
+while($dream = $result->fetch()) {
 ?>
+<div><a href="product.php?product=<?php echo $dream['name'] ?>"><?php echo $dream['name'] ?></a></div> <?php  
+};
+// Pagination
+$req = "SELECT COUNT(ID) AS totalprod FROM products";
+$result = $bdd->query($req);
+$count = $result->fetch(PDO::FETCH_ASSOC);
+$total_pages = ceil($count["totalprod"] / $results_per_page);
 
+//counter for pages (we only need 3 displayed pagination so I will do $i -1 | $i | $i + 1)
+?><ul><?php
 
+for ($i=1; $i<=$total_pages; $i++) { 
+    echo "<li><a href='index.php?page=".$i."'>".$i."</a></li>"; 
+};
+?>
+</ul>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
