6bd8749526dcfcae33306b90e340b7027c45b50c
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-11-15 19:12:09 +0100

    search

--- search.php
+++ search.php
@@ -1,9 +1,30 @@
 <?php
 require "bdd_pdo.php";
 
-$sql = "SELECT categories.name AS cat_name FROM categories";
+$sql = "SELECT id, categories.name AS cat_name FROM categories";
 $categories = $bdd->query($sql);
 
+var_dump($_POST);
+
+function sort_category($POST)
+{
+	$string = "";
+
+	if (isset($POST["category_id"]))
+	{
+		if ($POST["category_id"] == "all")
+		{
+			echo 'aaaaaa';
+			$string;
+		}
+
+		if ($POST["category_id"])
+		{
+			$string = "WHERE category_id = " . $POST["category_id"];
+		}
+	}
+	return $string;
+}
 
 if (isset($_POST["keywords"]))
 {
@@ -11,57 +32,50 @@ if (isset($_POST["keywords"]))
 
 	if ($keywords == "")
 	{
-		if (isset($_POST["alphabetical"]))
+		if (isset($_POST["price_sort"]))
 		{
-			if ($_POST["alphabetical"] == 'yes')
+			if ($_POST["price_sort"] == 'high')
 			{
-			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id ORDER BY products.name";
+			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id " . sort_category($_POST) . " ORDER BY price";
 			}
 			else
 			{
-			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id ORDER BY products.name DESC";
+			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id " . sort_category($_POST) . " ORDER BY price DESC";
 			}
 		}else
 		{
-			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id";
+			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id " . sort_category($_POST);
 		}
 	}else
 	{
-		if (isset($_POST["alphabetical"]))
+		if (isset($_POST["price_sort"]))
 		{
-			if ($_POST["alphabetical"] == 'yes')
+			if ($_POST["price_sort"] == 'low')
 			{
-				$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE products.name LIKE '%$keywords%' ORDER BY products.name";
+				$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE " . sort_category($_POST) . " products.name LIKE '%$keywords%' ORDER BY price";
 			}
 			else
 			{
-				$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE products.name LIKE '%$keywords%' ORDER BY products.name DESC";
+				$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE" . sort_category($_POST) . " products.name LIKE '%$keywords%' ORDER BY price DESC";
 			}
 		}else
 		{
-			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE products.name LIKE '%$keywords%'";
+			$sql = "SELECT products.name, price, categories.name AS cat_name FROM products JOIN categories ON category_id = categories.id WHERE " . sort_category($_POST) . "  products.name LIKE '%$keywords%'";
 		}
 	}
-	
+
+
 	$result_name = $bdd->query($sql);
 
-	try {
+	if ($result_name)
+	{
 		$count = $result_name->rowCount();
-	} catch (Exception $e) {
+	}
+	else
+	{
 		$count = 0;
 	}
 
-	function product_name_with_id($id)
-	{
-		$sql = "SELECT name FROM products WHERE id = " . $id;
-		$product = $bdd->query($sql);
-
-		$name = $product->fetch()["name"];
-
-		$product->closeCursor();
-
-		return $name;
-	}
 }
 ?>
 
@@ -76,11 +90,35 @@ if (isset($_POST["keywords"]))
 		<form method="post" action="search.php">
 			<label>Search</label>
 			<input type="text" name="keywords" placeholder="Type the dream name" value=<?php echo isset($_POST["keywords"]) ? $_POST["keywords"] : null; ?>>
-			
+		<!--
 			<p>
 				<label>Sort by name</label>
-				<input type="radio" name="alphabetical" value="yes" <?php echo ($_POST["alphabetical"] == "yes") ? "checked" : ""; ?>>Alphabetical order
-				<input type="radio" name="alphabetical" value="no" <?php echo ($_POST["alphabetical"] == "no") ? "checked" : ""; ?>>Reverse alphabetical order
+				<input type="radio" name="alphabetical" value="yes" <?php echo (isset($_POST["alphabetical"]) && $_POST["alphabetical"] == "yes") ? "checked" : ""; ?>>Alphabetical order
+				<input type="radio" name="alphabetical" value="no" <?php echo (isset($_POST["alphabetical"]) && $_POST["alphabetical"] == "no") ? "checked" : ""; ?>>Reverse alphabetical order
+			</p>
+		-->
+
+			<p>
+				<label>Sort by price</label>
+				<input type="radio" name="price_sort" value="high" <?php echo (isset($_POST["price_sort"]) && $_POST["price_sort"] == "high") ? "checked" : ""; ?>>High to low
+				<input type="radio" name="price_sort" value="low" <?php echo (isset($_POST["price_sort"]) && $_POST["price_sort"] == "low") ? "checked" : ""; ?>>Low to high
+			</p>
+
+			<p>
+				<label>Sort by category</label>
+				<select name="category_id"> 
+				<option value="all" <?php echo (isset($_POST["category_id"]) && $_POST["category_id"] == "all") ? "selected" : ""; ?>>All</option>
+				<?php
+					while ($data = $categories->fetch())
+					{
+						array_push($categories_name, $data["cat_name"]);
+				?>
+						<option value=<?php echo $data["id"]; ?> <?php echo (isset($_POST["category_id"]) && $_POST["category_id"] == $data["id"]) ? "selected" : ""; ?>><?php echo $data["cat_name"] ?></option>
+				<?php
+					}
+				?>
+			</select>
+				
 			</p>
 			<input type="submit" value="Search">
 		</form>
@@ -88,13 +126,16 @@ if (isset($_POST["keywords"]))
 	<div class="result">
 		<p>Found <?php echo $count; ?> result(s)</p>
 		<?php
-			while ($data = $result_name->fetch())
+			if ($count != 0)
 			{
-		?>
-				<h2><?php echo $data["name"] ;?></h2>
-				<p>Price : <?php echo $data["price"] ;?>$</p>
-				<p>Category : <?php echo $data["cat_name"] ;?></p>
-		<?php
+				while ($data = $result_name->fetch(PDO::FETCH_ASSOC))
+				{
+			?>
+					<h2><?php echo $data["name"] ;?></h2>
+					<p>Price : <?php echo $data["price"] ;?>$</p>
+					<p>Category : <?php echo $data["cat_name"] ;?></p>
+			<?php
+				}
 			}
 		?>
 
