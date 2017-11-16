d33304c67cb361790ffabbf5a33ce601f1219540
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-11-16 10:23:32 +0100

    login --> bug si déjà connexté fixed

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $loginform = new Form_User(array(
 	'email', 'password')
 );
 
-if (isset($_COOKIE["username"]))
+if (isset($_COOKIE["username"]) || isset($_SESSION["username"]))
 {
 	header("Location:./index.php");
 }
@@ -52,22 +52,35 @@ if (isset($_POST["email"]) && isset($_POST["password"]))
 <head>
 	<meta charset="UTF-8">
 	<title>Login Dream</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 </head>
 <body>
-	<p>If you have an account, log in:</p>
-	<form action="login.php" method="post">
-		<?php 
-			echo $loginform->input_text('email', isset($_POST['email']) ? $_POST['email'] : null);
-			echo $loginform->input_password('password');
-			echo $loginform->input_checkbox('remember_me', isset($_POST['remember_me']));
-			echo $loginform->submit('Envoyer');
-		?>
-	</form>
+
+	<div class="container">
+		<div class="row">
+			<p>If you have an account, log in:</p>
+			
+			<div class="col-s12">
+			<form action="login.php" method="post">
+			<?php 
+				echo $loginform->input_text('email', isset($_POST['email']) ? $_POST['email'] : null);
+				echo $loginform->input_password('password');
+				echo $loginform->input_checkbox('remember_me', isset($_POST['remember_me']));
+				echo $loginform->submit('Envoyer');
+			?>
+		</form>
+
+			</div>
+		</div>
+		
+		
 
 
-	<p>New customer</p>
-	<p>Register now for faster shopping next time you order. You can also manage your wish list, redeem e-vouchers and select preferences.</p>
-	<a href="./subscription.php">Register now</a>
+		<p>New customer</p>
+		<p>Register now for faster shopping next time you order. You can also manage your wish list, redeem e-vouchers and select preferences.</p>
+		<a href="./subscription.php">Register now</a>
+	</div>
+
 
 </body>
 </html>
